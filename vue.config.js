a63f4ba5aafbab7ddcdbe91d5c7f873963c9dc2b
2575861539@qq.com <2575861539@qq.com>
2024-10-08 16:34:31 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -12,13 +12,12 @@ module.exports = {
   // 部署应用包时的基本 URL,从 Vue CLI 3.3 起已弃用baseUrl
   // publicPath: process.env.NODE_ENV !== "production" ? "./" : '/',
   // publicPath: process.env.NODE_ENV !== "production" ? "/" : "/", //本地开发用
-  publicPath: process.env.NODE_ENV !== "production" ? "/" : "/", //本地开发用
+  publicPath: process.env.NODE_ENV !== "production" ? "/" : "/dist_pc", //本地开发用
 
   // build时构建文件的目录 构建时传入 --no-clean 可关闭该行为
-  outputDir: "dist",
+  outputDir: "dist_pc",
 
   // build时放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
-  // assetsDir: "dist_pc_hemeixin",
   assetsDir: "",
 
   // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
