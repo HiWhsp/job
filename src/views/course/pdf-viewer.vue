d408d7ffcdc5faadf7406f99f905b0b48e2835df
2575861539@qq.com <2575861539@qq.com>
2025-06-11 16:56:52 +0800

    111

--- src/views/course/pdf-viewer.vue
+++ src/views/course/pdf-viewer.vue
@@ -99,9 +99,15 @@ export default {
           width="100%"
           height="100%"
         ></iframe>
-        <div v-else>
-          <!-- 展示docs文件 -->
-        </div>
+        <!-- 展示docs文件 -->
+        <iframe
+          v-else
+          width="100%"
+          height="100%"
+          :src="`https://view.officeapps.live.com/op/view.aspx?src=${encodeURIComponent(
+            selectItem.file_path_url
+          )}`"
+        />
       </div>
     </div>
     <div class="right">
