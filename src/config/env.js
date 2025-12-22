55fc1089fdb255014d7d49bad60ce1e9e06c8cf3
2575861539@qq.com <2575861539@qq.com>
2025-12-22 13:19:26 +0800

    123

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 // import {API_ROOT} from '@/config/env.js'
 // const SITE_ROOT = 'https://yaojiandan.api.dx.hdapp.com.cn';
-const ROOT = 'https://yuanjinshu.dx.hdapp.com.cn/api';
-export const SITE_ROOT = 'https://yuanjinshu.dx.hdapp.com.cn/api';
+const ROOT = 'https://www.yuansteel.cn/api';
+export const SITE_ROOT = 'https://www.yuansteel.cn/api';
 export const API_ROOT = SITE_ROOT;
 export const UPLOAD_ROOT = process.env.NODE_ENV !== "production" ? SITE_ROOT + '/upload' : SITE_ROOT + '/upload';
 
